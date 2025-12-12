52fafabe2c6d67261c5d50f31cb2dede918e51a6
AlexFerras <78160840+AlexFerras@users.noreply.github.com>
2025-12-12 22:53:02 +0100

    hitboxes init commit

---
+++ melee_hitbox_debug_draw/scripts/mods/melee_hitbox_debug_draw/melee_hitbox_debug_draw_data.lua
@@ -0,0 +1,24 @@
+local mod = get_mod("melee_hitbox_debug_draw")
+
+return {
+	name = mod:localize("mod_name"),
+	description = mod:localize("mod_description"),
+	is_togglable = true,
+	options = {
+		widgets = {
+			{
+				setting_id = "lifetime",
+				type = "numeric",
+				default_value = 3,
+				range = {0, 60},
+				unit_text = "lifetime"
+			},
+			{
+				setting_id = "dodge",
+				type = "checkbox",
+				default_value = true
+
+			}
+		}
+	}
+}
---
+++ melee_hitbox_debug_draw/scripts/mods/melee_hitbox_debug_draw/melee_hitbox_debug_draw_localization.lua
@@ -0,0 +1,12 @@
+return {
+	mod_name = {
+		en = "Melee hitbox debug draw"
+	},
+	mod_description = {
+		en = "Draw enemies melee hitboxes",
+	},
+	lifetime = {
+		en = "LineObject lifetime"
+	}
+
+}
